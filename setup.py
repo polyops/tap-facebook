e6d11ff57ec5d134d516cd5356a643101fdc51fc
Manav <manavk123@gmail.com>
2020-06-16 19:00:20 -0700

    No ticket. Add fields and bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='polyops-tap-facebook',
-      version='0.0.4',
+      version='0.0.5',
       description='Singer.io tap for extracting data from the Facebook Ads API',
       author='Stitch',
       url='https://singer.io',
